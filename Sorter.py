ba9dc05f67bab3a370b30f8dfb30e68b8bd5ef80
Jamie Byers <102622047+JamieByers@users.noreply.github.com>
2023-08-15 17:30:42 +0100

    Sorter.py

--- Sorter.py
+++ Sorter.py
@@ -1,6 +1,22 @@
-def sort(dir):
+def sort(dir=None):
     import os
     
+        
+    if dir == None:
+        dir = input("Input Directort (desktop for Desktop, downloads for Downloads): ")
+        dir = dir.lower()
+
+    if dir == "desktop":
+        dir = os.path.expanduser("~/Desktop")
+    
+    elif dir == "downloads":
+        dir = os.path.expanduser("~/Downloads")
+        
+        
+    installers = os.path.expanduser("~/Desktop/Installers")
+    
+    
+    count = 0
     for file in os.listdir(dir):
         file = os.path.splitext(file)
         con_file = "".join(file)
@@ -8,13 +24,14 @@ def sort(dir):
         if "installer" in file[0].lower() or "setup" in file[0].lower():
             if file[1] != '':
                 print(file)
+                count += 1
                 
                 try:
-                    os.rename(dir + "/" + con_file, "C:/Users/jamie/Desktop/Installers" + "/" + con_file)
+                    os.rename(dir + "/" + con_file, installers + "/" + con_file)
                 except Exception as e:
                     print(f"{file} already exists Error: {e}")
-        
+
+    print("Number of files terminated: ", count)    
+    
 
         
-
-
